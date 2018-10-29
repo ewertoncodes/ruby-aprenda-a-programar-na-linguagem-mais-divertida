b4519f29ac418bcea7ab9ce0788d8b9fd7c5980e
ewertonorg <ewertonorg@gmail.com>
2018-10-29 01:43:08 -0300

    Capítulo 6 Explorando API File

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -2,25 +2,25 @@ class Biblioteca
   attr_reader :livros
 
   def initialize
-    @livros = {}
+    @banco_de_arquivos = BancoDeArquivos.new
   end
-  
+
   def adiciona(livro)
-    @livros[livro.categoria] ||= []
-    @livros[livro.categoria] << livro
+    salva livro do
+      livros << livro
+    end
+  end
+
+  def livros_por_categoria(categoria)
+    livros.select { |livro| livro.categoria == categoria }
   end
 
   def livros
-    @livros.values.flatten
-  end  
-  
-  # def adiciona(livro)
-  #   @livros << livro
-  # end
+    @livros ||= @banco_de_arquivos.carrega
+  end
 
-  def livros_por_categoria(categoria)
-    @livros[categoria].each do |livro|
-      yield livro if block_given?
-    end
-  end      
+  def salva(livro)
+    @banco_de_arquivos.salva livro
+    yield
+  end
 end
--- teste.rb
+++ teste.rb
@@ -155,7 +155,7 @@
 # puts nome # => LUCAS
 
 
-biblioteca = Biblioteca.new
-biblioteca.adiciona Livro.new "TDD", "Mauricio Aniche","123454", 247, 69.9, :testes
-biblioteca.adiciona Livro.new "Design Responsivo", "Tárcio Zemel", "45256", 189, 69.9, :web_design
-relatorio = Relatorio.new biblioteca
+# biblioteca = Biblioteca.new
+# biblioteca.adiciona Livro.new "TDD", "Mauricio Aniche","123454", 247, 69.9, :testes
+# biblioteca.adiciona Livro.new "Design Responsivo", "Tárcio Zemel", "45256", 189, 69.9, :web_design
+# relatorio = Relatorio.new biblioteca
